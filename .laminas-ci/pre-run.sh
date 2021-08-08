10a259387d9391fe63f26a6ca296f7acb9e44fa8
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-08-08 16:08:39 +0200

    qa: try to reinstall memcached only for PHP 8.0
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- .laminas-ci/pre-run.sh
+++ .laminas-ci/pre-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "no" | pecl install -f memcached
+JOB=$3
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
+
+if [ "$PHP_VERSION" = "8.0" ]; then
+    apt install libmemcached11
+    echo "/usr/lib/x86_64-linux-gnu\nno\nno\nyes\nno" | pecl install -f memcached
+fi
 
 echo -n "Memcached version: "
 php -r 'echo phpversion("memcached");'
